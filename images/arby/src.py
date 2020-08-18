70a0e0b58b6fefba96bed4ea7b618b69b6d2c22c
raladev <aladev.r@gmail.com>
2020-08-18 13:20:42 +0300

    Update src.py

--- images/arby/src.py
+++ images/arby/src.py
@@ -1,1 +1,9 @@
-REPO_URL = "https://github.com/ExchangeUnion/market-maker-tools"
+from tools.core import src
+
+
+class SourceManager(src.SourceManager):
+    def __init__(self):
+        super().__init__("https://github.com/ExchangeUnion/market-maker-tools")
+
+    def get_ref(self, version):
+            return "v1.0.0"
