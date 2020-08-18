977d3eaa11ff3dfc88f82e17ee6a6b3556094fe6
raladev <aladev.r@gmail.com>
2020-08-18 13:21:06 +0300

    Update src.py

--- images/connext/src.py
+++ images/connext/src.py
@@ -6,13 +6,4 @@ class SourceManager(src.SourceManager):
         super().__init__("https://github.com/connext/rest-api-client")
 
     def get_ref(self, version):
-        if version == "latest":
-            return "1f8f40af7a14a7050c260207283bc3477276312d"
-        elif version == "7.0.0":
-            return "5127ad848c3f598a9f23c51dc6d491753740832d"
-        elif version == "7.1.0":
-            return "26c521ac62181c1d1945a3e252351e24beee8096"
-        elif version == "7.1.1":
-            return "16fa7aecc19f44dab12e14b9264df63fccb25585"
-        elif version == "7.1.2":
-            return "1f8f40af7a14a7050c260207283bc3477276312d"
+            return "50291fef23b519bb78d0bd4a24e647330918cede"
